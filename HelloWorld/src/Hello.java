0d5e291fa59eeb8ed51ce9ddc764cec134e67d44
Van Phuong <van.phuong@maine.edu>
2016-01-25 09:54:40 -0500

    add code

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -1,4 +1,11 @@
 
 public class Hello {
+	/**
+	 * Implement the application
+	 * @param args agurment
+	 */
+	public static void main(String[] args){
+			System.out.println("Hello World 2");
+	}
 
 }

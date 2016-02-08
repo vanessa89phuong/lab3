d7a044e13e1e70be4edb1fdfd71f69c7d5cf2402
Van Phuong <van.phuong@maine.edu>
2016-02-08 09:32:34 -0500

    preamble
    Signed-off-by: Van Phuong <van.phuong@maine.edu>

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -5,9 +5,11 @@ public class Hello {
 	 * @param args agurment
 	 */
 	public static void main(String[] args){
+		System.out.println("something, something");
 		System.out.println("Here ye Here ye: Important announcement");
 			System.out.println("Hello World 2");
 			System.out.println("Good Bye");
+			
 			
 	}
 

a17f31343f532f58d26b678d419b151b931be141
Van Phuong <van.phuong@maine.edu>
2016-02-16 14:10:59 -0500

    this is master
    Signed-off-by: Van Phuong <van.phuong@maine.edu>

---
+++ githubTest/src/githubTest/testMain.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package githubTest;
+
+/**
+ * @author VanPhuong
+ *
+ */
+public class testMain {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Test for github");
+
+	}
+
+}

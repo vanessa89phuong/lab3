7ecd7eb91bf4d53692fded971770a251faa1dbdc
Van Phuong <van.phuong@maine.edu>
2016-02-22 18:49:35 -0500

    first commit

---
+++ lab3/src/lab3/main.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package lab3;
+
+/**
+ * @author VanPhuong
+ *
+ */
+public class main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hello world lab3");
+
+	}
+
+}
